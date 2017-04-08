06b18cd0b87549f6d26543e10598aa11b107a60d
yoshi252 <yoshi252>
2017-04-08 13:13:13 +0200

    Fix for windows

--- src/mikmod_driver/drv_retro_game.h
+++ src/mikmod_driver/drv_retro_game.h
@@ -6,11 +6,19 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define RETRO_AUDIO_BUFFER_SIZE (735 * 4) * 5
 extern uint8_t* retro_audioBuffer;
 extern uint32_t retro_bufferBytesRequested;
 extern uint32_t retro_bufferBytesWritten;
 
 extern MDRIVER drv_retrogame;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // DRV_RETRO_GAME_H
